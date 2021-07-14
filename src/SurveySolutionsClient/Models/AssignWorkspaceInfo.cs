4d98cdfcde95409296c3e0cd84e4df32510fbae8
VItaliy Balabanov <vitaliy.balabanov@gmail.com>
2021-07-14 15:06:04 -0400

    fix assign user to workspace

---
+++ src/SurveySolutionsClient/Models/AssignWorkspaceInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SurveySolutionsClient.Models
+{
+    public class AssignWorkspaceInfo
+    {
+        public AssignWorkspaceInfo()
+        {
+        }
+
+        public AssignWorkspaceInfo(string workspace, Guid? supervisorId = null)
+        {
+            Workspace = workspace;
+            SupervisorId = supervisorId;
+        }
+        
+        public string Workspace { get; set; }
+
+        public Guid? SupervisorId { get; set; }
+    }
+}
